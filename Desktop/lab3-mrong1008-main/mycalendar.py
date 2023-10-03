16b818c2df74e5217b065769d8cd7ff77aa68d69
mrong1008 <mrong@wm.edu>
2023-10-03 16:08:35 -0400

    completed mycalendar.py

--- Desktop/lab3-mrong1008-main/mycalendar.py
+++ Desktop/lab3-mrong1008-main/mycalendar.py
@@ -1,22 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-# 平年：365 天 ， 2月 28 天  2021 
-# 闰年：366 天 ， 2月 29 天  2020
-
-# 年份整除4 ——》 闰年
-# else -> 平年
-
-# 2100 ->平年
-
-# 某一个年份可以整除100， 那么只有当他可以整除400的时候，才是闰年
-
-# //
-# 1:31, 2:28/29, 3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31
-
 def is_leap_year(year):
     if year%100 == 0:
         if year %400 == 0:
@@ -28,7 +9,7 @@ def is_leap_year(year):
             return True
         else:
             return False
-def num_days_in_month(year, month):
+def num_days_in_month(year,month):
     if month == 2:
         if is_leap_year(year):
             return 29
@@ -58,6 +39,5 @@ def day_in_year_to_date(year, day):
         if day < num_days_in_month(year, month):
             return (year,month,day)
         day -= num_days_in_month(year, month)
-        if day == 0:
-            return (year,month,num_days_in_month(year, month))
 
+# assert is_leap_year(2000)==True
